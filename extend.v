685a6ed59dc964413a0ea0f2d88bb0cc1932769b
Tamilarasan555 <151667666+Tamilarasan555@users.noreply.github.com>
2024-08-29 10:58:47 +0200

    Add files via upload

--- extend.v
+++ extend.v
@@ -1,20 +1,15 @@
-module extent ( imm_ext, imm, imm_scr);
+module extend(in,Immext,immsrc);
+  input [31:7]in;
+  input [1:0]immsrc;
+  output [31:0]Immext;
+ 
+//   assign Immext = (immsrc == 1'b1) ? ({{20{in[31]}},in[31:25],in[11:7]}):
+//                                      {{20{in[31]}},in[31:20]};
+  
+  assign Immext = (immsrc == 2'b00) ? {{20{in[31]}}, in[31:20]} :
+                  (immsrc == 2'b01) ? {{20{in[31]}}, in[31:25], in[11:7]} :
+                  (immsrc == 2'b10) ? {{20{in[31]}}, in[7], in[30:25], in[11:8], 1'b0} :
+                  (immsrc == 2'b11) ? {{12{in[31]}}, in[19:12], in[20], in[30:21], 1'b0} : 32'hX;
 
-output [31:0] imm_ext;
-input [31:0] imm;
-input reg [2:0] imm_scr;
-
-
-always @ (*) begin
-
-	case(immsrc)
-	   3'b000 : immext = {20{imm[31]},imm[31:20]}; // I - Type
-	   3'b001 : immext = {20{imm[31]},imm[31:25],imm[11:7]}; // S - Type
-	   3'b011 : immext = {20{imm[31]},imm[7],imm[30:25], imm[11:8], 1'b0}; // B - Type
-	   3'b100 : immext = {12{imm[31],imm[19:12], imm[20], imm[30:25], imm[24:21], 1'b0}; // J - Type
-	   3'b101 : immext = {imm[31:12], 12'b_0000_0000_0000}; // U - Type
-	   default : immext = 32'hx;
-	endcase
-end
-
+  
 endmodule
