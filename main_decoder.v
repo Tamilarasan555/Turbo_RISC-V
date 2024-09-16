80d049a1cd35be03b9dc680146ab2c55c44ba988
Tamilarasan555 <151667666+Tamilarasan555@users.noreply.github.com>
2024-09-16 13:55:50 +0200

    Update main_decoder.v

--- main_decoder.v
+++ main_decoder.v
@@ -1,103 +1,105 @@
-module main_decoder(op,zero,pcsrc,resultsrc,memwrite,alusrc,immsrc,branch,regwrite,aluop);
- input zero;
-  input [6:0]op;
-  output   pcsrc;
-  output reg memwrite,alusrc,regwrite;
-  output reg [1:0]resultsrc;
-  output reg [1:0]immsrc;  
-  output reg [1:0]aluop;
- output reg branch;
-  reg jump;
+module main_decoder(op, zero, pc_src, result_src, mem_write, alu_src, imm_src, branch, reg_write, alu_op);
+	input zero;
+   input [6:0] op;
+   
+	output   pc_src;
+   output reg mem_write, alu_src, reg_write;
+   output reg [1:0] result_src;
+   output reg [1:0] imm_src;  
+   output reg [1:0] alu_op;
+   output reg branch;
+   
+	reg jump;
   
   always@(*)begin
     
-    if(op==7'b0000011) begin // lw I type
-      regwrite<=1'b1;
-      immsrc<=2'b00;
-      alusrc<=1'b1;
-      memwrite<=1'b0;
-      resultsrc<=2'b01;
-      branch<=1'b0;
-      aluop<=2'b00;
-      jump<=1'b0;
-    // pcsrc<=1'b0;
+    if(op == 7'b0000011) begin // lw I type
+      reg_write <= 1'b1;
+      imm_src <= 2'b00;
+      alu_src <= 1'b1;
+      mem_write <= 1'b0;
+      result_src <= 2'b01;
+      branch <= 1'b0;
+      alu_op <= 2'b00;
+      jump <= 1'b0;
+    // pcsrc <= 1'b0;
     end
     
-    else if(op==7'b0100011)begin //S type 
-       regwrite<=1'b0;
-      immsrc<=2'b01;
-      alusrc<=1'b1;
-      memwrite<=1'b1;
-      resultsrc<=2'b00;
-      branch<=1'b0;
-      aluop<=2'b00;
-      jump<=1'b0;
-  //   pcsrc<=1'b0;
-    end
-    
-    else if(op==7'b0110011)begin //R type
-           regwrite<=1'b1;
-      immsrc<=2'b00;
-      alusrc<=1'b0;
-      memwrite<=1'b0;
-      resultsrc<=2'b00;
-      branch<=1'b0;
-      aluop<=2'b10;
-      jump<=1'b0;
-    //   pcsrc<=1'b0;
-    end
-    
-    else if(op==7'b1100011)begin  //branch if equal
-      regwrite<=1'b0;
-      immsrc<=2'b10;
-      alusrc<=1'b0;
-      memwrite<=1'b0;
-      resultsrc<=2'b00;
-      branch<=1'b1;
-      aluop<=2'b01;
-      jump<=1'b0;
-  //    pcsrc<=1'b1;
-    end
-    
-    else if(op==7'b0010011) begin //I type addi
-    regwrite<=1'b1;
-      immsrc<=2'b00;
-      alusrc<=1'b1;
-      memwrite<=1'b0;
-      resultsrc<=2'b00;
-      branch<=1'b0;
-      aluop<=2'b00;
-      jump<=1'b0;
+    else if(op == 7'b0100011)begin //S type 
+      reg_write <= 1'b0;
+      imm_src <= 2'b01;
+      alu_src <= 1'b1;
+      mem_write <= 1'b1;
+      result_src <= 2'b00;
+      branch <= 1'b0;
+      alu_op <= 2'b00;
+      jump <= 1'b0;
    //   pcsrc<=1'b0;
+    end
+    
+    else if(op == 7'b0110011)begin //R type
+      reg_write <= 1'b1;
+      imm_src <= 2'b00;
+      alu_src <= 1'b0;
+      mem_write <= 1'b0;
+      result_src <= 2'b00;
+      branch <= 1'b0;
+      alu_op <= 2'b10;
+      jump <= 1'b0;
+    //  pc_src<=1'b0;
+    end
+    
+    else if(op == 7'b1100011)begin  //branch if equal
+      reg_write <= 1'b0;
+      imm_src <= 2'b10;
+      alu_src <= 1'b0;
+      mem_write <= 1'b0;
+      result_src <= 2'b00;
+      branch <= 1'b1;
+      alu_op <= 2'b01;
+      jump <= 1'b0;
+  //    pcsrc <= 1'b1;
+    end
+    
+    else if(op == 7'b0010011) begin //I type addi
+      reg_write <= 1'b1;
+      imm_src <= 2'b00;
+      alu_src <= 1'b1;
+      mem_write <= 1'b0;
+      result_src <= 2'b00;
+      branch <= 1'b0;
+      alu_op <= 2'b00;
+      jump <= 1'b0;
+   //   pcsrc <= 1'b0;
      end   
     
     
-    else if(op==7'b1101111) begin //jump instruction
-    regwrite<=1'b1;
-      immsrc<=2'b11;
-      alusrc<=1'b1;
-      memwrite<=1'b0;
-      resultsrc<=2'b10;
-      branch<=1'b0;
-      aluop<=2'b00;
-      jump<=1'b1;
-    //  pcsrc<=1'b1;
+    else if(op == 7'b1101111) begin //jump instruction
+      reg_write <= 1'b1;
+      imm_src <= 2'b11;
+      alu_src <= 1'b1;
+      mem_write <= 1'b0;
+      result_src <= 2'b10;
+      branch <= 1'b0;
+      alu_op <= 2'b00;
+      jump <= 1'b1;
+    //  pc_src <= 1'b1;
      end   
     
     else begin
-     regwrite<=1'b0;
-      immsrc<=2'b00;
-      alusrc<=1'b0;
-      memwrite<=1'b0;
-      resultsrc<=2'b00;
-      branch<=1'b0;
-      aluop<=2'b00;
-      jump<=1'b0;
-  //    pcsrc<=1'b0;
+      reg_write <= 1'b0;
+      imm_src <= 2'b00;
+      alu_src <= 1'b0;
+      mem_write <= 1'b0;
+      result_src <= 2'b00;
+      branch <=1'b0;
+      alu_op <= 2'b00;
+      jump <= 1'b0;
+  //    pc_src <= 1'b0;
     end
     
   end
   
-  assign pcsrc=((zero&branch)|jump);
+  assign pc_src=((zero & branch)|jump);
   
 endmodule
