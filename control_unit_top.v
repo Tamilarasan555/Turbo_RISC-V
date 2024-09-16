55c567f10e38b8f9cf07eb8ce26464ab9d473dd9
Tamilarasan555 <151667666+Tamilarasan555@users.noreply.github.com>
2024-09-16 13:57:58 +0200

    Update control_unit_top.v

--- control_unit_top.v
+++ control_unit_top.v
@@ -1,36 +1,39 @@
 
-module control_unit_top(op,zero,immsrc,regwrite,alusrc,memwrite,resultsrc,pcsrc,branch,funct3,funct7,alucontrol);
+module control_unit_top( op, zero, imm_src, reg_write, alu_src, mem_write, result_src, pc_src, branch, funct3, funct7, alu_control);
 
-  input [6:0]op,funct7;
-    input [2:0]funct3;
-  input zero;
-    output regwrite,alusrc,memwrite,branch,pcsrc;
-  output [1:0]immsrc,resultsrc;
-  output [2:0]alucontrol;
-  wire [1:0]aluop;
+	input [6:0] op, funct7;
+	input [2:0] funct3;
+	input zero;
+   
+	output reg_write, alu_src, mem_write, branch, pc_src;
+	output [1:0] imm_src, result_src;
+	output [2:0] alu_control;
+	
+	wire [1:0] alu_op;
 
    
   
-// Instantiate main_decoder module
+// Instantiating main_decoder module
   main_decoder main_decoder1(
-    .op(op),
-    .zero(zero),
-    .pcsrc(pcsrc),
-    .resultsrc(resultsrc),
-    .memwrite(memwrite),
-    .alusrc(alusrc),
-    .immsrc(immsrc),
-    .branch(branch),
-    .regwrite(regwrite),
-    .aluop(aluop)
+    .op (op),
+    .zero (zero),
+    .pc_src (pc_src),
+    .result_src (result_src),
+    .mem_write (mem_write),
+    .alu_src (alu_src),
+    .imm_src (imm_src),
+    .branch (branch),
+    .reg_write (reg_write),
+    .alu_op (alu_op)
   );
 
-  // Instantiate alu_decoder module
+ 
+// Instantiate alu_decoder module
   alu_decoder alu_decoder1(
-    .funct7(funct7),
-    .funct3(funct3),
-    .aluop(aluop),
-    .alucontrol(alucontrol)
+    .funct7 (funct7),
+    .funct3 (funct3),
+    .alu_op (alu_op),
+    .alu_control (alu_control)
   );
 
 endmodule
